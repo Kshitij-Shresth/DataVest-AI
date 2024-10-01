4547392ea82b94b172e270d81b0993a48b6240c4
Kshitij Shresth <kshitijshresth@icloud.com>
2024-10-01 07:48:19 +0300

    Update loader.py
    recursive splitter

--- loader.py
+++ loader.py
@@ -4,3 +4,8 @@ nltk.download('punkt')
 url = "https://www.apple.com/newsroom/2024/08/apple-reports-third-quarter-results/"  
 loader = UnstructuredURLLoader(urls=[url])
 documents = loader.load()
+
+#Splitting text into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,   
+    chunk_overlap=100    
