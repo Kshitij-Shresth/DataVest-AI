d096300ab9ffdd4f1eee6353d763e3adb709b439
Kshitij Shresth <kshitijshresth@icloud.com>
2024-10-02 12:16:06 +0300

    Update Query.py

--- Query.py
+++ Query.py
@@ -1,1 +1,9 @@
+#Function to handle all user queries
+def ask_question(question):
+    docs = vector_store.similarity_search(question, k=1)
+    if not docs:
+        return "I'm sorry, I couldn't find any relevant information to answer your question."
+    context = " ".join([doc.page_content for doc in docs])
+    response = qa_model(question=question, context=context)
 
+    return response['answer']
